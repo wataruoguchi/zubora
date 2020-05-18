abce5308da2c753784474cfd8cb8f538ab95404a
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2020-05-18 00:03:37 -0700

    minor - get code for debugging

--- packages/zubora/src/index.ts
+++ packages/zubora/src/index.ts
@@ -4,8 +4,18 @@ import { transformer } from './transformer';
 import { traverser } from './traverser';
 import { template } from './template';
 
-function buildNewContent(srcPath: string, destPath: string, ast: File): string {
+function buildNewContent(
+  srcPath: string,
+  destPath: string,
+  ast: File,
+  code: string
+): string {
   const { exportedModules, classObjects } = traverser(ast);
+  if (!exportedModules) {
+    throw new Error(
+      `exportedModules got lost. ${JSON.stringify(exportedModules)} ${code}`
+    );
+  }
   const generateTemplate = template(srcPath, destPath);
   return prettier.format(generateTemplate(exportedModules, classObjects), {
     parser: 'babel',
@@ -19,8 +29,8 @@ async function zubora(
 ): Promise<string> {
   const result = await transformer(srcPath)(code);
   if (result && typeof result.code === 'string' && isFile(result.ast)) {
-    const { ast } = result;
-    const content = buildNewContent(srcPath, destPath, ast);
+    const { code, ast } = result;
+    const content = buildNewContent(srcPath, destPath, ast, code);
     return content;
   } else {
     return '// Hmm.. Something is wrong.';
