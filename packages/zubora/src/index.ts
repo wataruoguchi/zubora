95bc6e6b4523592a539eb92bb4fda5bad48fb3fa
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2020-05-17 23:29:23 -0700

    We can get AST from transformer.

--- packages/zubora/src/index.ts
+++ packages/zubora/src/index.ts
@@ -3,7 +3,6 @@ import { isFile, File } from '@babel/types';
 import { transformer } from './transformer';
 import { traverser } from './traverser';
 import { template } from './template';
-import { parser } from './parser';
 
 function buildNewContent(srcPath: string, destPath: string, ast: File): string {
   const { exportedModules, classObjects } = traverser(ast);
@@ -20,11 +19,8 @@ async function zubora(
 ): Promise<string> {
   const result = await transformer(srcPath)(code);
   if (result && typeof result.code === 'string' && isFile(result.ast)) {
-    const { code, ast } = result;
-    // TODO Let's see the difference. ast vs ast2. It looks like they are different, but not sure how it affects. Probably we only need ast so we can remove parser.
-    const ast2 = parser(code);
-    let content = buildNewContent(srcPath, destPath, ast);
-    content += buildNewContent(srcPath, destPath, ast2);
+    const { ast } = result;
+    const content = buildNewContent(srcPath, destPath, ast);
     return content;
   } else {
     return '// Hmm.. Something is wrong.';
