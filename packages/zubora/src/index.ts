473bae7bf9ee4746868634c22717f8f1412f2970
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2020-05-18 18:48:25 -0700

    fix the code, cleaning up

--- packages/zubora/src/index.ts
+++ packages/zubora/src/index.ts
@@ -4,18 +4,8 @@ import { transformer } from './transformer';
 import { traverser } from './traverser';
 import { template } from './template';
 
-function buildNewContent(
-  srcPath: string,
-  destPath: string,
-  ast: File,
-  code: string
-): string {
+function buildNewContent(srcPath: string, destPath: string, ast: File): string {
   const { exportedModules, classObjects } = traverser(ast);
-  if (!exportedModules) {
-    throw new Error(
-      `exportedModules got lost. ${JSON.stringify(exportedModules)} ${code}`
-    );
-  }
   const generateTemplate = template(srcPath, destPath);
   return prettier.format(generateTemplate(exportedModules, classObjects), {
     parser: 'babel',
@@ -29,8 +19,8 @@ async function zubora(
 ): Promise<string> {
   const result = await transformer(srcPath)(code);
   if (result && typeof result.code === 'string' && isFile(result.ast)) {
-    const { code, ast } = result;
-    const content = buildNewContent(srcPath, destPath, ast, code);
+    const { ast } = result;
+    const content = buildNewContent(srcPath, destPath, ast);
     return content;
   } else {
     return '// Hmm.. Something is wrong.';
