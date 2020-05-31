d3146f05ced620f86dd50c989f38eb7867c07824
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2020-05-30 20:32:22 -0700

    Installed tailwind and build the first page

--- packages/zubora-page/pages/api/hello.ts
+++ packages/zubora-page/pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default (req, res) => {
+export default (req: NextApiRequest, res: NextApiResponse): void => {
   console.log(req);
   res.statusCode = 200;
   res.json({ name: 'John Doe' });
