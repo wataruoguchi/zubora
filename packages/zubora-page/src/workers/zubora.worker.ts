0770af9443bf0596638d6e50f4b40bf5945018d2
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2020-06-05 20:51:09 -0700

    Managed to work zubora-worker

---
+++ packages/zubora-page/src/workers/zubora.worker.ts
@@ -0,0 +1,24 @@
+import { zubora } from 'zubora';
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
+
+const ctx: Worker = self as any;
+ctx.postMessage({ foo: 'foo' });
+
+// Respond to message from parent thread
+ctx.addEventListener('message', async ({ data }) => {
+  const srcPath = 'src.ts';
+  if (data && typeof data === 'string') {
+    try {
+      const result = await zubora(srcPath, `./${srcPath}`, 'src', data);
+      const formattedResult = prettier.format(result, {
+        parser: 'babel',
+        plugins: [parserBabel],
+      });
+      ctx.postMessage(formattedResult);
+    } catch (err) {
+      ctx.postMessage(JSON.stringify(err));
+    }
+  }
+});
+export default ctx;
